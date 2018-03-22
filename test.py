759f110671031c5d19c12a0d72d23f4f8bc02ad6
unknown <kouxinyu@derastorage.com>
2018-03-22 20:21:01 +0800

    ==

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import os
+import steward_lib
 
 def get_current_node_info():
     list_pci_commamd = "lspci | grep 'Non-Volatile'"
-    nvme_pci_device = steward_lib.linux_command_output_to_list(list_pci_command)
+    nvme_pci_device = steward_lib.linux_command_output_to_list(list_pci_commamd)
     return nvme_pci_device
 
 print(get_current_node_info())
