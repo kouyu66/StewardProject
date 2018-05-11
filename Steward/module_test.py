852e7ea989cae7edffb9e88f6d6dff719d3dd819
kouyu66 <kouyu66@163.com>
2018-05-11 21:02:41 +0800

    重整函数结构

--- Steward/module_test.py
+++ Steward/module_test.py
@@ -1,17 +1,45 @@
 #!/usr/bin/env python3
 # Coding:UTF-8
 import os
-import time
+import re
 
-def env_check():
-    # install dmidecode newer than v2.8
-    # copy nvme tool
-    # add script to rc.
-    os.popen('cp -rf /system_repo/tools/dmidecode-3-1 /ge/auto')
-    os.popen('cp -rf /system_repo/tools/nvme/nvme .')
-    time.sleep(1)
-    os.chdir('/ge/auto/dmidecode-3-1')
-    os.popen('make && make install')
+def process_normal_mode(normal_cards_sn):
     
+    def process_script_change(info):
+        last_list, current_list = info
+        if not last_list:
+            if current_list[-1] == '1':  # 父进程为init进程，则当前进程由系统开机启动
+                pass
+            else:
+                pass    # 该进程非开机启动
+        if not current_list:
+            
+
+    for sn in normal_cards_sn:
+        current_trace = [trace for trace in current_traces if trace['SN'] == sn][0]
+        old_trace = [trace for trace in old_traces if trace['SN'] == sn][0]
+        head_info = {'now_time': now_time,'SN': sn}
+        key_info = {}
+
+        for key in current_trace:
+            if key in old_trace and current_trace[key] != old_trace[key]:
+                key_info[key] = [old_trace[key], current_trace[key]]
+                else:
+                    continue
+        if not key_info:
+            head_info['info_type': 'heartbeat']
+        else:
+            head_info['info_type': 'normal_update']
+            head_info.update(key_info)
+        
+        # 增加对脚本启动时间的判断
+        if head_info.get('script'):
+            info = head_info.get('script')
+            process_script_change(info)
+
+
+
+        json_info = json.dumps(key_info)
+        send_info(json_info)
+        # pass    # send heartbeat.
     return
-env_check()
