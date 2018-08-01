123c8a9c0f38970a2ae3c058ab118220cd7c8f97
kouyu66 <kouyu66@163.com>
2018-08-01 18:18:28 +0800

    对多线程函数添加备注信息

--- steward_lib.py
+++ steward_lib.py
@@ -142,7 +142,8 @@ def selectFromList(list):
     return user_select_item
 
 def multiThread(funcname, listname):
-    """以列表中元素作为参数，列表元素数作为线程个数，多线程执行指定函数"""
+    """以列表中元素作为参数，列表元素数作为线程个数，多线程执行指定函数
+    阻塞，当所有线程执行完成后才继续主线程"""
     threadIndex = range(len(listname))
     threads = []
     for num in threadIndex :
@@ -155,6 +156,8 @@ def multiThread(funcname, listname):
     return
 
 def multiThreadDeamon(funcname, listname):
+    '''以列表中元素作为参数，列表元素个数作为线程数，多线程执行指定函数
+    非阻塞，主线程不等待子线程执行结束，且主线程退出时，子线程立即退出'''
     threadIndex = range(len(listname))
     threads = []
     for num in threadIndex :
