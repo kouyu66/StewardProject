37b844bce8011cbfa53e1850b11157d801158efc
kouyu66 <kouyu66@163.com>
2018-08-01 18:18:55 +0800

    添加多线程相关函数

--- basic_lib/lib.py
+++ basic_lib/lib.py
@@ -3,6 +3,7 @@
 
 # 从列表中选择一项
 def select_from_list(list_obj):
+    '''根据用户输入，选择列表中元素（单选）'''
     if not list_obj:    # 空列表检查
         print('empty list detected. no selection is needed.')
         return 1
@@ -24,3 +25,29 @@ def select_from_list(list_obj):
     except Exception:   # 非法字符检查
         print('invalid input detected.')
         return 1
+
+def multiThread(funcname, listname):
+    """以列表中元素作为参数，列表元素数作为线程个数，多线程执行指定函数
+    阻塞，当所有线程执行完成后才继续主线程"""
+    threadIndex = range(len(listname))
+    threads = []
+    for num in threadIndex :
+        t = threading.Thread(target=funcname, args=(listname[num],))
+        threads.append(t)
+    for num in threadIndex:
+        threads[num].start()
+    for num in threadIndex:
+        threads[num].join()
+    return
+
+def multiThreadDeamon(funcname, listname):
+    '''以列表中元素作为参数，列表元素个数作为线程数，多线程执行指定函数
+    非阻塞，主线程不等待子线程执行结束，且主线程退出时，子线程立即退出'''
+    threadIndex = range(len(listname))
+    threads = []
+    for num in threadIndex :
+        t = threading.Thread(target=funcname, args=(listname[num],), daemon=True)
+        threads.append(t)
+    for num in threadIndex:
+        threads[num].start()
+    return
