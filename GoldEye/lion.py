7594c045a480682ff16a2488103f70793cfae4fd
kouxinyu_Mac <kouyu66@163.com>
2018-09-20 11:15:27 +0800

    github clean operation

--- GoldEye/lion.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*
-# Author: kouxinyu
-# Email: kouyu66@163.com
-# Version: 0.0.1
-# 主控脚本，循环搜索制定网段可监控的测试机，并将dokkaebi脚本植入，
-# 同时将dokkaebi反馈的信息填入数据库，并响应客户端的查询请求。
